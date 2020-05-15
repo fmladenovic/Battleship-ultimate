cd483f2b09544e2f678a71a0c90da028c8f1e6fe
Filip <filipeng97@gmail.com>
2020-05-15 19:06:08 +0200

    Resoner configured

--- backend/battleship/src/main/java/com/sbz/battleship/BattleshipApplication.java
+++ backend/battleship/src/main/java/com/sbz/battleship/BattleshipApplication.java
@@ -1,7 +1,11 @@
 package com.sbz.battleship;
 
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BattleshipApplication {
@@ -10,4 +14,17 @@ public class BattleshipApplication {
         SpringApplication.run(BattleshipApplication.class, args);
     }
 
+    @Bean
+    public KieContainer kieContainer() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("com.sbz", "resoner", "0.0.1-SNAPSHOT"));
+        KieScanner kScanner = ks.newKieScanner(kContainer);
+        kScanner.start(1000);
+
+//        KieSession kieSession = kContainer.newKieSession("session");
+//        kieSession.addEventListener(new DebugAgendaEventListener());
+//        kieSession.setGlobal("triggeredAlarmService", triggeredAlarmService);
+        return kContainer;
+    }
 }
